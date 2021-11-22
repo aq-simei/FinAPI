de62d24cb8c5baeb7f62a37258bdd2a99924232e
Antonio Simei <aq.simei@gmail.com>
2021-11-22 17:50:35 -0300

    Acess to user's statement

--- src/index.js
+++ src/index.js
@@ -28,4 +28,13 @@ app.post('/account', (request, response)=>{
 
   return response.status(201).send();
 });
+
+app.get("/statement/:cpf", (request, response) => {
+  const {cpf} = request.params;
+
+  const costumer = costumers.find(costumer => costumer.cpf === cpf );
+  return response.json(costumer.statement)
+});
+
+
 app.listen(3333);
